9cb076e4dcab571e21af44119b4fca562410fc7b
bmlindelwa <3441209@myuwc.ac.za>
2020-05-08 23:38:52 +0200

    Updates

---
+++ precache-manifest.aac2f7e5fff2b5458edeefbb90aee6de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab59b1b1b99cf21abc09edd3bb506c17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "70ba3197b239a97ee327",
+    "url": "/static/css/main.72b65a39.chunk.css"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ba3197b239a97ee327",
+    "url": "/static/js/main.10e3d3ba.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
